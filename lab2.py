3a7f715aa8225fbc7a9424b1a24d179e61441caa
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2020-10-20 00:13:04 +0200

    asgfdg

--- lab2.py
+++ lab2.py
@@ -1,5 +1,15 @@
-txt = "hej hopp"
-
-x = txt.split()
-
-print(x)
+f = open("score2.txt", "r")
+d = {}
+for line in f.readlines():
+    line = line.split()
+    firstName = line[2]
+    lastName = line[3]
+    fullName = firstName + lastName
+    score = line[4]
+    d[fullName] = score
+    for fullName, score in d.items():
+        if score not in fullName:
+            
+        else:
+            d[fullName] = d.get(fullName, 0) + int(score)
+print(d)
